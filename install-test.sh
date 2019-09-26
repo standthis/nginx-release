ecc73d30f190e82e841b635d6dda84009d5f8a23
standthis <standthis@protonmail.ch>
2019-09-26 11:30:56 +0200

    refine install-test

--- install-test.sh
+++ install-test.sh
@@ -6,27 +6,27 @@ pushd .
 echo "-----> `date`: Install director VM if non-existent"
 if [ ! -e ~/deployments/vbox ]; then
     git clone https://github.com/cloudfoundry/bosh-deployment ~/workspace/bosh-deployment
-    mkdir -p ~/deployments/vbox
-    cd ~/deployments/vbox
-
-    bosh create-env ~/workspace/bosh-deployment/bosh.yml \
-      --state ./state.json \
-      -o ~/workspace/bosh-deployment/virtualbox/cpi.yml \
-      -o ~/workspace/bosh-deployment/virtualbox/outbound-network.yml \
-      -o ~/workspace/bosh-deployment/bosh-lite.yml \
-      -o ~/workspace/bosh-deployment/bosh-lite-runc.yml \
-      -o ~/workspace/bosh-deployment/uaa.yml \
-      -o ~/workspace/bosh-deployment/credhub.yml \
-      -o ~/workspace/bosh-deployment/jumpbox-user.yml \
-      --vars-store ./creds.yml \
-      -v director_name=bosh-lite \
-      -v internal_ip=192.168.50.6 \
-      -v internal_gw=192.168.50.1 \
-      -v internal_cidr=192.168.50.0/24 \
-      -v outbound_network_name=NatNetwork
 fi
 
+mkdir -p ~/deployments/vbox
 cd ~/deployments/vbox
+
+echo "-----> `date`: Create environment for BOSH-lite"
+bosh create-env ~/workspace/bosh-deployment/bosh.yml \
+  --state ./state.json \
+  -o ~/workspace/bosh-deployment/virtualbox/cpi.yml \
+  -o ~/workspace/bosh-deployment/virtualbox/outbound-network.yml \
+  -o ~/workspace/bosh-deployment/bosh-lite.yml \
+  -o ~/workspace/bosh-deployment/bosh-lite-runc.yml \
+  -o ~/workspace/bosh-deployment/uaa.yml \
+  -o ~/workspace/bosh-deployment/credhub.yml \
+  -o ~/workspace/bosh-deployment/jumpbox-user.yml \
+  --vars-store ./creds.yml \
+  -v director_name=bosh-lite \
+  -v internal_ip=192.168.50.6 \
+  -v internal_gw=192.168.50.1 \
+  -v internal_cidr=192.168.50.0/24 \
+  -v outbound_network_name=NatNetwork
 
 echo "-----> `date`: Set environemnt variables and credentials"
 export BOSH_CLIENT=admin
